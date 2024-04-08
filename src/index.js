f51229b86c95db6e1ddc8643722fde3b29cc7cf3
Abdul Wahab Asif <abdulwahab5547@gmail.com>
2024-04-08 11:44:47 +0500

    connected database

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import connectDB from "./db/index.js";
+
+const app = express()
+
+connectDB()
+
+
+/*
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error: ", error)
+            throw error 
+        })
+
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error: ", error)
+        throw err 
+    }
+})()
+
+*/
